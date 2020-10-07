bee774aea1c1810892e342e82df5d6917bf0698c
David Chege <dkc.ushift@gmail.com>
2020-10-06 20:35:52 -0500

    "Routes Middleware Folder Created"

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+/* ----------------------- Internal---------- */
+router.get('title/:product_Id');
+router.get('brand/:product_Id');
+
+/* ----------------------- External---------- */
+router.get();
